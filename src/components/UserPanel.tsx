dfadc0fa40d673c97e088fd523c8d318a1d8617b
Basile Vernouillet <basile64.v@gmail.com>
2023-03-25 16:39:06 +0100

    fix: hydration issue

--- src/components/UserPanel.tsx
+++ src/components/UserPanel.tsx
@@ -1,29 +1,38 @@
 'use client';
 
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 import Button from './Button';
 import { usePocket } from './PocketContext';
 
 function UserPanel() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [avatar, setAvatar] = useState('');
+
   const { user, logout, pb } = usePocket();
 
-  const avatar = user
-    ? pb.getFileUrl(user, user.avatar, {
-        thumb: '32x32',
-      })
-    : '';
+  useEffect(() => {
+    if (user) {
+      setIsLoggedIn(true);
+      setAvatar(
+        pb.getFileUrl(user, user.avatar, {
+          thumb: '32x32',
+        }),
+      );
+    }
+  }, []);
 
   return (
     <div>
-      {user ? (
+      {isLoggedIn ? (
         <div className="flex gap-2">
           <Button variant="secondary" onClick={logout}>
             Logout
           </Button>
           {avatar && (
-            <div className="h-8 w-8 rounded-full overflow-hidden border border-sky-600">
-              <img className="w-full h-full object-cover" src={avatar} alt="avatar" />
+            <div className="w-8 h-8 overflow-hidden border rounded-full border-sky-600">
+              <img className="object-cover w-full h-full" src={avatar} alt="avatar" />
             </div>
           )}
         </div>
