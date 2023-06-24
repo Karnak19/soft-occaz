cae90f710233ecc1c13432a4c7e34c8654bc29ef
Basile Vernouillet <basile64.v@gmail.com>
2023-06-24 12:23:33 +0200

    feat: add 404 page

--- src/app/annonces/details/[id]/page.tsx
+++ src/app/annonces/details/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
 
 import ProductDetails from '$/components/details/ProductDetails';
 import { prisma } from '$/utils/db';
@@ -6,15 +7,19 @@ import { getSingleAd } from '$/utils/getters/getSingleAd';
 import sanitizer from '$/utils/sanitizer';
 
 export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
-  const ad = await prisma.listing.findUniqueOrThrow({ where: { id: params.id } });
+  try {
+    const ad = await prisma.listing.findUniqueOrThrow({ where: { id: params.id } });
 
-  return {
-    title: ad.title,
-    description: sanitizer(ad.description).substring(0, 150),
-    openGraph: {
-      images: [{ url: ad.images[0] }],
-    },
-  };
+    return {
+      title: ad.title,
+      description: sanitizer(ad.description).substring(0, 150),
+      openGraph: {
+        images: [{ url: ad.images[0] }],
+      },
+    };
+  } catch (error) {
+    return notFound();
+  }
 }
 
 async function page({ params }: { params: { id: string } }) {
---
+++ src/app/not-found.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+function notfound() {
+  return (
+    <main className="relative isolate h-[calc(100vh-74px)]">
+      <Image src="/404.jpg" fill alt="" quality={50} className="-z-10 blur-sm h-full w-full object-cover object-top" />
+      <div className="mx-auto max-w-7xl px-6 py-12 text-center sm:py-20 lg:px-8">
+        <p className="text-base font-semibold leading-8 text-black">404</p>
+        <h1 className="mt-4 text-3xl font-bold tracking-tight text-black sm:text-5xl">Page not found</h1>
+        <p className="mt-4 text-base text-black/70 sm:mt-6">Sorry, we couldn’t find the page you’re looking for.</p>
+        <div className="mt-10 flex justify-center">
+          <Link href="#" className="text-sm font-semibold leading-7 text-black">
+            <span aria-hidden="true">&larr;</span> Back to home
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default notfound;
