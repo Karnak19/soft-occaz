92c58517ffc041fd36c7bb8f33cfd18789abaf75
Basile Vernouillet <basile64.v@gmail.com>
2023-07-07 20:39:38 +0200

    fix: use Next Image for CTA section

--- src/components/home/RegisterCTA.tsx
+++ src/components/home/RegisterCTA.tsx
@@ -82,7 +82,7 @@ export default function CTA() {
         <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
           <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
             <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-              <img
+              <Image
                 src="/hero2.jpg"
                 alt="App screenshot"
                 width={2432}
