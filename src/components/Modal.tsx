9b8c5b0f70cfc0176bc077c00bb7ca9144cfceca
Basile Vernouillet <basile64.v@gmail.com>
2023-07-22 13:12:01 +0200

    feat: add framer-motion to the modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,6 +3,7 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 
+import { motion } from 'framer-motion';
 import { useRouter } from 'next/navigation';
 import { MouseEventHandler, useCallback, useEffect, useRef } from 'react';
 
@@ -45,13 +46,23 @@ export default function Modal({ children }: { children: React.ReactNode }) {
   }, [onKeyDown]);
 
   return (
-    <div ref={overlay} className="fixed grid place-items-center z-30 inset-0 bg-black/60" onClick={onClick}>
-      <div
+    <motion.div
+      ref={overlay}
+      className="fixed grid place-items-center z-30 inset-0 bg-black/60"
+      onClick={onClick}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <motion.div
         ref={wrapper}
         className="rounded overflow-auto absolute bg-gray-100 w-11/12 max-h-[calc(100vh-4rem)] sm:w-10/12 md:w-8/12 p-6"
+        initial={{ y: 100, opacity: 0, scale: 0.1 }}
+        animate={{ y: 0, opacity: 1, scale: 1 }}
+        exit={{ y: 100, opacity: 0 }}
       >
         {children}
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 }
