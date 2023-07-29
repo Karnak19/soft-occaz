9c160945ddae24935fb65ca0323d0ea36b2edf06
Basile Vernouillet <basile64.v@gmail.com>
2023-07-29 19:34:07 +0200

    fix: update sitemap with annonces and profiles

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,11 +1,21 @@
+import { prisma } from '$/utils/db';
 import { Type } from '@prisma/client';
 import { MetadataRoute } from 'next';
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const BASE_URL = 'https://airsoft-market.store';
 
   const types = Object.values(Type).map((type) => {
     return { url: `${BASE_URL}/annonces/${type.toLowerCase()}`, lastModified: new Date() };
+  });
+
+  const annonces = await prisma.listing.findMany({
+    select: { id: true, updatedAt: true },
+    where: { sold: false },
+  });
+
+  const users = await prisma.user.findMany({
+    select: { id: true, updatedAt: true },
   });
 
   return [
@@ -16,5 +26,13 @@ export default function sitemap(): MetadataRoute.Sitemap {
     { url: `${BASE_URL}/search`, lastModified: new Date() },
     { url: `${BASE_URL}/privacy-policy`, lastModified: new Date() },
     ...types,
+    ...annonces.map((annonce) => ({
+      url: `${BASE_URL}/annonces/details/${annonce.id}`,
+      lastModified: annonce.updatedAt,
+    })),
+    ...users.map((user) => ({
+      url: `${BASE_URL}/profile/${user.id}`,
+      lastModified: user.updatedAt,
+    })),
   ];
 }
