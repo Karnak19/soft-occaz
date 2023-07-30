806ac345be8c9a8edb54d33464a6a90c95177f96
Basile Vernouillet <basile64.v@gmail.com>
2023-07-30 03:58:50 +0200

    fix: remove runtime from search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,8 +2,6 @@ import LastAds from '$/components/home/LastAds';
 import SearchSection from '$/components/home/SearchSection';
 import SearchList from '$/components/SearchList';
 
-export const runtime = 'edge';
-
 function Page({ searchParams }: { searchParams: { q: string } }) {
   return (
     <>
