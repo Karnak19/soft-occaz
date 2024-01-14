694f5f04bbf1356ee08c421ed69d00a6d4247b7b
basile <basile.vernouillet@origins-digital.com>
2024-01-14 19:20:37 +0100

    Refactor pricing section UI

--- src/components/PricingSection.tsx
+++ src/components/PricingSection.tsx
@@ -5,6 +5,8 @@ import { RadioGroup } from '@headlessui/react';
 import { CheckIcon } from '@heroicons/react/20/solid';
 import { cn } from '$/utils/cn';
 import Link from 'next/link';
+import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
+import { Button } from './ui/button';
 
 const frequencies = [
   { value: 'monthly', label: 'Mensuel', priceSuffix: '/mois' },
@@ -64,13 +66,12 @@ export default function PricingSection() {
   const [frequency, setFrequency] = useState(frequencies[0]);
 
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div className="py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-4xl text-center">
-          <h2 className="text-base font-semibold leading-7 text-rg-500 ">Abonnements</h2>
-          <p className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">Abonnements pour les particuliers</p>
+          <p className="mt-2 text-4xl font-bold tracking-tight text-foreground sm:text-5xl">Abonnements pour les particuliers</p>
         </div>
-        <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600">
+        <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-muted-foreground">
           Choisissez l&apos;abonnement qui convient le mieux à vos besoins. Vous pouvez changer d&apos;abonnement à tout moment.
         </p>
         <div className="mt-16 flex justify-center">
@@ -85,7 +86,10 @@ export default function PricingSection() {
                 key={option.value}
                 value={option}
                 className={({ checked }) =>
-                  cn(checked ? 'bg-rg-500 text-white' : 'text-gray-500', 'cursor-pointer rounded-full px-2.5 py-1')
+                  cn(
+                    checked ? 'bg-rg-500 dark:bg-primary text-background' : 'text-muted-foreground',
+                    'cursor-pointer rounded-full px-2.5 py-1',
+                  )
                 }
               >
                 <span>{option.label}</span>
@@ -95,67 +99,71 @@ export default function PricingSection() {
         </div>
         <div className="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 md:max-w-2xl md:grid-cols-2 lg:max-w-4xl xl:mx-0 xl:max-w-none xl:grid-cols-4">
           {tiers.map((tier) => {
-            const ringColor = cn({
-              'ring-2 ring-amber-400': tier.id === 'tier-premium',
-              'ring-1 ring-violet-400': tier.id === 'tier-geardo',
-              'ring-1 ring-teal-400': tier.id === 'tier-hobby',
-              'ring-1 ring-gray-200': tier.id === 'tier-free',
-            });
-
-            const bgColor = cn({
-              'bg-gradient-to-tr from-amber-100': tier.id === 'tier-premium',
-              'bg-violet-50': tier.id === 'tier-geardo',
-              'bg-teal-50': tier.id === 'tier-hobby',
-              'bg-white': tier.id === 'tier-free',
-            });
-
-            const textColor = cn({
-              'text-amber-600': tier.id === 'tier-premium',
-              'text-violet-600': tier.id === 'tier-geardo',
-              'text-teal-600': tier.id === 'tier-hobby',
-              'text-gray-600': tier.id === 'tier-free',
-            });
-
-            const linkCn = cn({
-              'bg-amber-600 text-white shadow-sm hover:bg-amber-700': tier.id === 'tier-premium',
-              'bg-white text-rg-700 ring-1 ring-inset shadow-sm hover:bg-violet-200': tier.id === 'tier-geardo',
-              'bg-white text-rg-700 ring-1 ring-inset shadow-sm hover:bg-teal-100': tier.id === 'tier-hobby',
-              'bg-white text-rg-700 ring-1 ring-inset shadow-sm hover:bg-gray-200': tier.id === 'tier-free',
-            });
+            const colorMap = {
+              'tier-premium': {
+                ring: 'ring-2 ring-amber-500 dark:ring-amber-400',
+                bg: 'bg-gradient-to-b via-transparent from-amber-300/50',
+                buttonClassNames: null,
+                text: 'text-amber-600',
+                buttonVariant: 'premium',
+              },
+              'tier-geardo': {
+                ring: null,
+                bg: 'bg-gradient-to-b via-transparent from-violet-400/30',
+                buttonClassNames: 'bg-violet-400 text-black',
+                text: 'text-violet-600',
+                buttonVariant: 'secondary',
+              },
+              'tier-hobby': {
+                ring: null,
+                bg: 'bg-gradient-to-b via-transparent from-teal-400/30',
+                buttonClassNames: 'bg-teal-400 text-black',
+                text: 'text-teal-600',
+                buttonVariant: 'secondary',
+              },
+              'tier-free': {
+                ring: null,
+                bg: null,
+                buttonClassNames: null,
+                text: 'text-gray-600',
+                buttonVariant: 'ghost',
+              },
+            } as const;
 
             return (
-              <div key={tier.id} className={cn(ringColor, bgColor, 'rounded-3xl p-8')}>
-                <h3 id={tier.id} className={cn(textColor, 'text-lg font-semibold leading-8')}>
-                  {tier.name}
-                </h3>
-                <p className="mt-4 text-sm leading-6 text-gray-600">{tier.description}</p>
-                <p className="mt-6 flex items-baseline gap-x-1">
-                  <span className="text-4xl font-bold tracking-tight text-gray-900">{tier.price[frequency.value]}</span>
-                  <span className="text-sm font-semibold leading-6 text-gray-600">{frequency.priceSuffix}</span>
-                </p>
-                <Link
-                  href={tier.href}
-                  aria-describedby={tier.id}
-                  className={cn(
-                    linkCn,
-                    'mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rg-500 ',
-                  )}
-                >
-                  {tier.cta}
-                </Link>
-                <ul role="list" className="mt-8 space-y-3 text-sm leading-6 text-gray-600">
-                  {tier.features.map((feature) => (
-                    <li key={feature} className="flex gap-x-3">
-                      <CheckIcon className="h-6 w-5 flex-none text-rg-500 " aria-hidden="true" />
-                      {feature}
-                    </li>
-                  ))}
-                </ul>
-              </div>
+              <Card key={tier.id} className={cn(colorMap[tier.id].ring, colorMap[tier.id].bg)}>
+                <CardHeader>
+                  <CardTitle className="text-2xl">{tier.name}</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  {tier.description && <p className="mt-4 text-sm leading-6 text-gray-600">{tier.description}</p>}
+                  <p className="flex items-baseline gap-x-1">
+                    <span className="text-4xl font-bold tracking-tight">{tier.price[frequency.value]}</span>
+                    <span className="text-sm font-semibold leading-6 ">{frequency.priceSuffix}</span>
+                  </p>
+                  <Button
+                    variant={colorMap[tier.id].buttonVariant}
+                    asChild
+                    className={cn('block mt-6 text-center text-sm font-semibold', colorMap[tier.id].buttonClassNames)}
+                  >
+                    <Link href={tier.href} aria-describedby={tier.id}>
+                      {tier.cta}
+                    </Link>
+                  </Button>
+                  <ul role="list" className="mt-8 space-y-3 text-sm leading-6 text-muted-foreground">
+                    {tier.features.map((feature) => (
+                      <li key={feature} className="flex gap-x-3">
+                        <CheckIcon className="h-6 w-5 flex-none text-accent-foreground" aria-hidden="true" />
+                        {feature}
+                      </li>
+                    ))}
+                  </ul>
+                </CardContent>
+              </Card>
             );
           })}
         </div>
-        <p className="mx-auto mt-6 max-w-3xl text-center italic leading-8 text-rg-300">
+        <p className="mx-auto mt-12 text-pretty max-w-3xl text-center italic leading-8 text-rg-300 dark:text-primary/50">
           Pourquoi un système d&apos;abonnement ? Airsoft-Market est un projet sur lequel je travaille seul, sur mon temps libre
           et personnel. Les abonnements me permettent de financer du café, mais aussi et surtout les frais d&apos;hébergement.
           Merci pour votre soutien !
