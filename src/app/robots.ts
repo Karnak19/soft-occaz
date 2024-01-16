d3aedac73a1553a0c142bcc3ed8b1d9c80d055d3
basile <basile.vernouillet@origins-digital.com>
2024-01-16 13:24:53 +0100

    fix: Add sitemap URL to robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,5 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/_next/', '/dashboard/'],
     },
+    sitemap: 'https://airsoft-market.store/sitemap.xml',
   };
 }
