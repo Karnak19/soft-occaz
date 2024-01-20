52b9e335cb2d7563c326377d7694c5a8f953b512
basile <basile.vernouillet@origins-digital.com>
2024-01-20 23:36:19 +0100

    fix: Update secret comparison in checkSecret function

--- src/utils/check-secret.ts
+++ src/utils/check-secret.ts
@@ -1,7 +1,7 @@
 import { env } from '$/env';
 
 export function checkSecret(secret?: string | null) {
-  const isOk = secret === env.CLERK_SECRET_KEY;
+  const isOk = secret === env.WEBHOOK_SECRET;
   if (!isOk) {
     throw new Error('Invalid secret');
   }
