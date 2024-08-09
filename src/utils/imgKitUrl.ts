66635c9e10527ababcf1490dd227b2df83ed6a3a
basile <basile.vernouillet@origins-digital.com>
2024-08-09 13:23:36 +0200

    fix: no images uploaded

--- src/utils/imgKitUrl.ts
+++ src/utils/imgKitUrl.ts
@@ -1,17 +1,17 @@
-export function imgKitUrl(url: string) {
-  const isImgKit = url.startsWith('https://ik.imagekit.io');
+export function imgKitUrl(url: string | undefined) {
+  const isImgKit = url?.startsWith('https://ik.imagekit.io');
 
-  return isImgKit ? `${url}?tr=w-800,q-100` : url;
+  return isImgKit ? `${url}?tr=w-800,q-100` : url ?? '/placeholder.webp';
 }
 
-export function imgKitUrlThumbnail(url: string) {
-  const isImgKit = url.startsWith('https://ik.imagekit.io');
+export function imgKitUrlThumbnail(url: string | undefined) {
+  const isImgKit = url?.startsWith('https://ik.imagekit.io');
 
-  return isImgKit ? `${url}?tr=w-420,q-95` : url;
+  return isImgKit ? `${url}?tr=w-420,q-95` : url ?? '/placeholder.webp';
 }
 
-export function imgKitUrlLow(url: string) {
-  const isImgKit = url.startsWith('https://ik.imagekit.io');
+export function imgKitUrlLow(url: string | undefined) {
+  const isImgKit = url?.startsWith('https://ik.imagekit.io');
 
-  return isImgKit ? `${url}?tr=w-80,q-20` : url;
+  return isImgKit ? `${url}?tr=w-80,q-20` : url ?? '/placeholder.webp';
 }
